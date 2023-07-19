253fe2bff7c6af9eea1c788742863290849bb247
minazaher <menazaher115@gmail.com>
2023-07-19 22:03:08 +0300

    migrating orderhistory to viewpager2

--- app/src/main/java/com/example/ecbabywear/UI/OrderHistory/OrderHistoryFragment.java
+++ app/src/main/java/com/example/ecbabywear/UI/OrderHistory/OrderHistoryFragment.java
@@ -1,55 +1,66 @@
 package com.example.ecbabywear.UI.OrderHistory;
 
-
-import static com.example.ecbabywear.ApplicationClass.firebaseAuth;
-
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.ecbabywear.Model.Order;
 import com.example.ecbabywear.R;
-import com.example.ecbabywear.Repositories.OrderRepository;
-import com.example.ecbabywear.Utilis.OrdersAdapter;
-import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OrderHistoryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class OrderHistoryFragment extends Fragment {
-    private OrderRepository orderRepository;
-    private ArrayList<Order> confirmedOrders;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public OrderHistoryFragment() {
+        // Required empty public constructor
+    }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment OrderHistoryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OrderHistoryFragment newInstance(String param1, String param2) {
+        OrderHistoryFragment fragment = new OrderHistoryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        orderRepository = new OrderRepository();
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_order_history, container, false);
-
-        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
-        orderRepository.getConfirmedOrdersByUser(firebaseUser, orders -> {
-            RecyclerView ordersRecyclerView = (RecyclerView) view.findViewById(R.id.orders_recylerview);
-            confirmedOrders = (ArrayList<Order>) orders;
-            OrdersAdapter adapter = new OrdersAdapter((ArrayList<Order>) orders, this.getContext());
-            ordersRecyclerView.setAdapter(adapter);
-            ordersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        });
-
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_history2, container, false);
     }
 }
